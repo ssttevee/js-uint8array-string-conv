2ca6e631f4e977ad03c73165f6c200795aa0b14d
Steve Lam <steviewc@pm.me>
2019-08-26 22:34:52 -0700

    fix string conversion with unicode characters

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 export function stringToArray(s: string): Uint8Array {
-    return new Uint8Array(s.split('').map((c) => c.charCodeAt(0)))
+    const utf8 = unescape(encodeURIComponent(s));
+    return Uint8Array.from(utf8, (_, i) => utf8.charCodeAt(i));
 }
 
 export function arrayToString(a: Uint8Array): string {
-    return Array.from(a, (c) => String.fromCharCode(c)).join('');
+    const utf8 = Array.from(a, (c) => String.fromCharCode(c)).join('');
+    return decodeURIComponent(escape(utf8));
 }
 
 export function mergeArrays(...arrays: Uint8Array[]): Uint8Array {
