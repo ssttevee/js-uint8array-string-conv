cd63eb27535f0949d4dbf9ddcb1b3327cd4c6dbf
Steve Lam <stevyo99@yahoo.ca>
2020-08-11 12:39:29 -0700

    add `arraysEqual`

--- src/index.ts
+++ src/index.ts
@@ -19,3 +19,17 @@ export function mergeArrays(...arrays: Uint8Array[]): Uint8Array {
 
     return out;
 }
+
+export function arraysEqual(a: Uint8Array, b: Uint8Array): boolean {
+    if (a.length !== b.length) {
+        return false;
+    }
+
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] !== b[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
